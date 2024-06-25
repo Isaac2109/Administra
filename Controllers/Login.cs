eb36f4f9c09020110a9db849646aed6fae054c63
Isaac2109 <113056042+Isaac2109@users.noreply.github.com>
2024-06-24 22:03:02 -0300

    Contexto BD finalizado, tela de login iniciada

---
+++ Controllers/Login.cs
@@ -0,0 +1,20 @@
+﻿using Administra.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Administra.Controllers
+{
+    public class Login : Controller
+    {
+        [HttpGet]
+        public IActionResult Logar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Logar(Usuario objeto)
+        {
+            return View();
+        }
+    }
+}
